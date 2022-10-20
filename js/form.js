9e10fb424fbdb859c7e92dc89e5c927b96789695
Dmitry Lutsishin <graf.luck@gmail.com>
2022-10-20 14:23:16 +0300

    feat: add reset form data

--- js/form.js
+++ js/form.js
@@ -2,7 +2,16 @@ const photoUploadButton = document.querySelector('#upload-file');
 const photoEditForm = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const cancelFormButton = document.querySelector('#upload-cancel');
+const scalePhotoText = document.querySelector('.scale__control--value');
+const previewPhotoImg = document.querySelector('.img-upload__preview img');
+const descriptionPhotoText = document.querySelector('.text__description');
 
+const resetFormData = () => {
+  photoUploadButton.value = '';
+  scalePhotoText.value = '100%';
+  previewPhotoImg.classList.replace('effects__preview--none');
+  descriptionPhotoText.textContent = '';
+};
 
 const openModal = () => {
   photoEditForm.classList.remove('hidden');
@@ -15,7 +24,7 @@ const openModal = () => {
 const closeModal = () => {
   photoEditForm.classList.add('hidden');
   body.classList.remove('modal-open');
-  photoUploadButton.value = '';
+  resetFormData();
   cancelFormButton.removeEventListener('click', onCancelFormButtonClick);
   document.removeEventListener('keydown', onModalEscKeydown);
   photoUploadButton.addEventListener('change', onUploadPhotoButtonChange);
