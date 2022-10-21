d98fe168a9ee3461e6c5bff894fdf3fc8e0654da
Dmitry Lutsishin <graf.luck@gmail.com>
2022-10-21 16:27:38 +0300

    feat: started slider implementation

--- js/form.js
+++ js/form.js
@@ -92,4 +92,16 @@ effectsList.addEventListener('change', (evt) => {
   }
 });
 
+const sliderElement = document.querySelector('.effect-level__slider');
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+  step: 1,
+  connect: 'lower',
+});
+
 export {createEventFormHandlers};
