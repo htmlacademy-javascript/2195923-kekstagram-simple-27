9789c15985f1b4c196fad572922e9b64d2583587
Dmitry Lutsishin <graf.luck@gmail.com>
2022-11-01 09:07:45 +0300

    fix: delete unused code

--- js/util.js
+++ js/util.js
@@ -1,34 +1,3 @@
-const generateRandomInteger = (from, to) => {
-  if (typeof from !== 'number' || typeof to !== 'number') {
-    return NaN;
-  }
-
-  if (from < 0 || to < 0) {
-    return NaN;
-  }
-
-  return Math.round(Math.random() * Math.abs(to - from) + from);
-};
-
-const checkMaxStringLength = (string, maxLength) => {
-  if (typeof string !== 'string' || typeof maxLength !== 'number' || maxLength < 0) {
-    return null;
-  }
-  return string.length <= maxLength;
-};
-
-const shuffleArray = (items) => {
-  for (let i = items.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    const tmp = items[i];
-    items[i] = items[j];
-    items[j] = tmp;
-  }
-  return items;
-};
-
-const generateString = (strings) => strings[generateRandomInteger(0, strings.length - 1)];
-
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
   alertContainer.style.zIndex = '100';
@@ -62,4 +31,4 @@ const replaceClass = (object, newClass) => {
   object.classList.add(newClass);
 };
 
-export { generateRandomInteger, checkMaxStringLength, generateString, shuffleArray, showAlert, classEffectToIdEffect, idEffectToClassEffect, replaceClass };
+export { showAlert, classEffectToIdEffect, idEffectToClassEffect, replaceClass };
